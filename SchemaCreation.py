7ebbcc22df431baf97b6dd05983d6943ca5ec384
steviedas <steviedas>
2023-01-31 13:22:15 +0000

    Created Bike and Date Tables
    Created the Bike and the Date Tables

--- SchemaCreation.py
+++ SchemaCreation.py
@@ -204,3 +204,72 @@ silver_stations_df.write.format("delta").mode("overwrite").save("/tmp/Steven/Sil
 silver_riders_df.write.format("delta").mode("overwrite").save("/tmp/Steven/Silver/riders")
 
 silver_payments_df.write.format("delta").mode("overwrite").save("/tmp/Steven/Silver/payments")
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Read all the data from Silver into dataframe
+
+# COMMAND ----------
+
+silver_trips_delta_df = spark.read.format("delta").load("/tmp/Steven/Silver/trips/")
+silver_stations_delta_df = spark.read.format("delta").load("/tmp/Steven/Silver/stations/")
+silver_riders_delta_df = spark.read.format("delta").load("/tmp/Steven/Silver/riders/")
+silver_payments_delta_df = spark.read.format("delta").load("/tmp/Steven/Silver/payments/")
+
+# COMMAND ----------
+
+display(silver_trips_delta_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Create the Bike Table
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+from pyspark.sql.window import Window
+
+# Define the window specification
+w1 = Window.orderBy("rideable_type")
+
+bike_df = silver_trips_delta_df.select("rideable_type").distinct().withColumn("bike_id", F.row_number().over(w1)).select("bike_id","rideable_type")
+
+display(bike_df)
+
+# COMMAND ----------
+
+# MAGIC %md
+# MAGIC ##Create the Date Table
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+
+start_date_df = silver_trips_delta_df.select("started_at").withColumnRenamed("started_at","date")
+end_date_df = silver_trips_delta_df.select("ended_at").withColumnRenamed("ended_at","date")
+
+# Define the window specification
+w2 = Window.orderBy("date")
+
+# Merging two columns "col1" and "col2" into a new column "merged_col"
+date_df = start_date_df.union(end_date_df).distinct()    #.withColumn("date_id", F.row_number().over(w2)).select("date_id","date")
+print(date_df)
+display(date_df)
+
+# COMMAND ----------
+
+from pyspark.sql import functions as F
+from pyspark.sql.functions import col, split
+
+# Split the "merged_col" column by the delimiter "T "
+date_df = date_df.withColumn("date", col("date").cast("string"))
+date_df1 = date_df.withColumn('date_ext', split(col('date'), ' ')[0].substr(1, 11))
+date_df1 = date_df1.withColumn('time_ext', split(col('date'), ' ')[1].substr(0,5))
+
+display(date_df1)
+
+# from pyspark.sql.functions import split, col 
+# merged_dates1 = merged_dates.withColumn('date', split(col('started_at'), ' ')[0])
+# merged_dates1 = merged_dates1.withColumn('time', split(col('started_at'), ' ')[1].substr(0,5))
